eefc1cb6fc53211e87fe3f7410defa3179cc7948
PixelGuy75 <crafterguy75@gmail.com>
2015-03-21 20:45:00 -0500

    Initial Commit

---
+++ src/PixelGuy75/RankDisplay/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PixelGuy75\RankDisplay;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+use _64FF00\PurePerms\commands\SetGroup;
+
+class Main extends PluginBase implements Listener{
+	
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+	
+    public function onDisable(){
+    }
+	
+    public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$group = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUser($player)->getGroup();
+		$groupname = $group->getName();
+        $displayrank = "[" . $groupname . "] " . $player->getName();
+        $player->setNameTag($displayrank);
+    }
+	
+    public function onRankChange(SetGroup $event){
+		$player = $event->getPlayer();
+		$group = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUser($player)->getGroup();
+		$groupname = $group->getName();
+        $displayrank = "[" . $groupname . "] " . $player->getName();
+        $player->setNameTag($displayrank);
+    }
+}
